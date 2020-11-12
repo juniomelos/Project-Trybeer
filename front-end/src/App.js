eb876dfe6fc46143f96822fce9438f74ce16958b
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-11-12 15:38:21 -0300

    done except axios put request

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,54 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { loadFromLocalStorage } from './services/localStorage';
 import Login from './pages/Login/Login';
+import { login } from './store/ducks/user';
 import Register from './pages/Register/Register';
 import ClientProfile from './pages/ClientProfile/ClientProfile';
+import jwt_decode from 'jwt-decode';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+
+  const requireAuth = () => {
+    const userData = loadFromLocalStorage('user');
+    if (userData != null) {
+      const decoded = jwt_decode(userData.token);
+      console.log('token decoded', decoded);
+      const now = Date.now().valueOf() / 1000;
+      console.log("now", now, decoded.exp);
+      if (typeof decoded.exp !== 'undefined' && decoded.exp > now) {
+        const user = {
+          name: userData.name,
+          email: userData.email,
+          role: userData.role,
+        };
+        dispatch(login({ token: userData.token, user }));
+        return true;
+      }
+    }
+    return false;
+  };
+
   return (
     <Router>
       <Switch>
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
-        <Route exact path="/profile" component={ClientProfile} />
+        <Route
+          exact
+          path="/profile"
+          render={() =>
+            requireAuth() ? <ClientProfile /> : <Redirect to="/login" />
+          }
+        />
       </Switch>
     </Router>
   );
--- front-end/src/components/FormClientProfile.jsx
+++ front-end/src/components/FormClientProfile.jsx
@@ -1,13 +1,59 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { userLogin } from '../store/ducks/user';
+
 
 
 const FormClientProfile = () => {
+
+  // Set all local Action/Reducers
+  const userData = useSelector((state) => state.userReducer.user);
+
+  const [user, setUser] = useState({
+    name: userData.name,
+  });
+
+  const [inputsValid, setInputsValid] = useState(
+    true,
+  );
+
+  function handleClick() {
+    console.log('handleClick clicked');
+  }
+
+  // Each time user is updated name is checked if are valid 
+  useEffect(() => {
+    const regexName = /\w{12}/i;
+
+    if (regexName.test(user.name) && user.name != userData.name) {
+      setInputsValid(false)
+    } else {
+      setInputsValid(true)
+    }
+  }, [user]);
+
   return (
+    <div>
 
-    <h1>
-      Here is FormClientProfile
+      <h1>
+        Here is FormClientProfile
     </h1>
-
+      <h2>
+        Seu email  :
+    </h2>
+      <h3 data-testid="profile-email-input"> {userData.email}</h3>
+      <form className="formContainer">
+        <input name="name" type="text"
+          data-testid="profile-name-input"
+          placeholder="Digit seu nome"
+          value={user.name}
+          onChange={(event) =>
+            setUser({ ...user, [event.target.name]: event.target.value })
+          }
+        />
+        <button data-testid="profile-save-btn" type="submit" onClick={handleClick} disabled={inputsValid}>CADASTRAR</button>
+      </form>
+    </div>
   )
 }
 
