9f4458b21c20ea4f50898c3d7f44fc2b8d009f96
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-11-15 10:55:34 -0300

    first commit

--- front-end/src/components/ProductsContainer.jsx
+++ front-end/src/components/ProductsContainer.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 
 const ProductsContainer = () => {
+const productList = [{
+  name: 'Skol Lata 250ml',
+  price: 2.20, 
+  link: 'http://localhost:3001/images/Skol Lata 350ml.jpg'
+},
+{
+  name: 'Heineken 600ml',
+  price: 7.70, 
+  link: 'http://localhost:3001/images/Heineken 600ml.jpg'
+},
+]
 
   return (
     <>
