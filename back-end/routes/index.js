7f2fbe00a57d9c78ca932feb6ef2ff5d1ee10a6b
juniomelos <juniomelos@gmail.com>
2020-11-16 15:38:33 -0300

    alteraçoes na rota do products

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,9 +1,11 @@
 const { loginRouter } = require('./loginRouter');
 const { registerRouter } = require('./registerRouter');
 const { profileRouter } = require('./profileRouter');
+const { productsRouter } = require('./productsRouter');
 
 module.exports = {
   loginRouter,
   registerRouter,
   profileRouter,
+  productsRouter,
 };
