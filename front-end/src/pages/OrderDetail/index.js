8d228454f531327f7315a3a0cadf43d2c796587f
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-11-23 17:45:10 -0300

    add total price

--- front-end/src/pages/OrderDetail/index.js
+++ front-end/src/pages/OrderDetail/index.js
@@ -7,32 +7,29 @@ const OrderDetail = (props) => {
 
   let newDate = '';
   let dateAndMonth = '';
-  console.log('props.match.params', props.match.params, props);
-  //   newDate = new Date(order.date),
-  //   dateAndMonth = `${newDate.getDate()}/${newDate.getMonth() + 1}`,
+
   return (
     <>
       <Header />
       <h3 data-testid="order-number">Pedido {props.match.params.id}</h3>
       <h3 data-testid="order-date">{props.location.state.date}</h3>
-      <h3>R$ {props.location.state.totalPrice}</h3>
       {/* 
-            <div className='cardContainer' data-testid={`${i}-order-card-container`}>
-              <h3 data-testid={`${i}-order-number`} >Pedido {order.id}</h3>
-              <h3 data-testid={`${i}-order-date`}>{
-                dateAndMonth
-              }</h3>
-              <h3 data-testid={`${i}-order-total-value`}>R$ {order.total.toFixed(2).toString().replace('.', ',')}</h3>
-              </div>
-
-      {
-         orders.map((order, i) => (
-          newDate = new Date(order.date),
-          dateAndMonth = `${newDate.getDate()}/${newDate.getMonth() + 1}`,
- 
-        )
-        )
-      } */}
+            <div className='cardContainer' key={name}>
+            <h3 data-testid={`${i}-product-qtd`} > {quantity} - </h3>
+            <h3 data-testid={`${i}-product-name`} > {name} - </h3>
+            <h3 data-testid={`${i}-product-total-value`} > {name} - </h3>
+            
+            </div>
+            
+            {
+                orders.map((order, i) => (
+                    newDate = new Date(order.date),
+                    dateAndMonth = `${newDate.getDate()}/${newDate.getMonth() + 1}`,
+                    
+                    )
+                    )
+                } */}
+      <h3>R$ {props.location.state.totalPrice}</h3>
     </>
   );
 };
